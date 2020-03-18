85951d5ccbce628a99d8cea206361fcc5c8f9523
Ben Lardie <benlardie@gmail.com>
2020-03-18 19:03:16 -0400

    table set up with all the data

--- src/Components/AllCountries.js
+++ src/Components/AllCountries.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 
 export default function AllCountries() {
     const [countriesData, setCountriesData] = useState([])
-    const {flag, code, name, countries} = require('country-emoji');
+    const { flag } = require('country-emoji');
     useEffect(() => {
         fetch('https://coronavirus-19-api.herokuapp.com/countries ')
           .then((response) => {
@@ -15,12 +15,38 @@ export default function AllCountries() {
 
     return (
         <div>
-            {countriesData.map(country => {
-                console.log(country)
+            <table>
+    <thead>
+        <tr>
+            <th colSpan="1">Country Name</th>
+            <th colSpan="1">Cases</th>
+            <th colSpan="1">Cases Today</th>
+            <th colSpan="1">Deaths</th>
+            <th colSpan="1">Deaths Today</th>
+            <th colSpan="1">Recovered</th>
+            <th colSpan="1">Active</th>
+            <th colSpan="1">Critical</th>
+        </tr>
+    </thead>
+    <tbody>
+    {countriesData.map(countryObject => {
+                const {country, cases, todayCases, deaths, todayDeaths, recovered, active, critical} = countryObject
                 return (
-                <h1>{country.country}{flag(country.country)}</h1>
+                <tr>
+                <td>{country}{flag(country)}</td>
+                <td>{cases}</td>
+                <td>{todayCases}</td>
+                <td>{deaths}</td>
+                <td>{todayDeaths}</td>
+                <td>{recovered}</td>
+                <td>{active}</td>
+                <td>{critical}</td>
+                </tr>
                 )
             })}
+    </tbody>
+</table>
+
         </div>
     )
 }
