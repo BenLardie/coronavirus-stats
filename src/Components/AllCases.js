b2f6c63d2ffe97b28d42b1d65204b6513454e186
Ben Lardie <benlardie@gmail.com>
2020-03-18 14:55:57 -0400

    moved all cases to its own component

---
+++ src/Components/AllCases.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const AllCases = (props) => {
+    return (
+        <>
+            <h1>Cases</h1>
+            <h2>{props.data.cases}</h2>
+            <h1>Deaths</h1>
+            <h1>{props.data.deaths}</h1>
+            <h1>Recovered</h1>
+            <h2>{props.data.recovered}</h2>
+        </>
+    )
+}
--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import { AllCases } from './AllCases';
 
 function App() {
   const [data, setData] = useState({})
@@ -26,12 +27,7 @@ function App() {
   console.log(countriesData)
   return (
     <>
-    <h1>Cases</h1>
-    <h2>{data.cases}</h2>
-    <h1>Deaths</h1>
-    <h1>{data.deaths}</h1>
-    <h1>Recovered</h1>
-    <h2>{data.recovered}</h2>
+      <AllCases data={data} />
     </>
   );
 }
