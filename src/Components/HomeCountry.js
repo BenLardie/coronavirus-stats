1f0dc80b362d2ea7a20d259a31f5bd77d075ccb7
Ben Lardie <benlardie@gmail.com>
2020-03-22 14:30:54 -0400

    getting specific data for home country

--- src/Components/HomeCountry.js
+++ src/Components/HomeCountry.js
@@ -1,16 +1,30 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function HomeCountry() {
 
+    const [countryName, setCountryName] = useState(null)
+    const [countryStats, setCountryStats]= useState(null)
+    
     useEffect(()=> {
         fetch('http://ip-api.com/json')
         .then((response) => {
           return response.json();
         })
         .then((data) => {
-          console.log(data)
+          setCountryName(data.country)
         });
     },[])
+    
+    useEffect(()=> {
+        fetch(`https://coronavirus-19-api.herokuapp.com/countries/${countryName}`)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          setCountryStats(data)
+        });
+    },[countryName])
+    console.log(countryStats)
     return (
         <div>
             
