ebb6536effce9bea53afb4abc141ce3cd1bba68e
Ben Lardie <benlardie@gmail.com>
2020-05-05 18:37:49 -0400

    changed it to todays date for current news selection

--- src/Components/News.js
+++ src/Components/News.js
@@ -2,7 +2,13 @@ import React, {useEffect} from 'react'
 
 export default function News() {
     const key =process.env.REACT_APP_API_KEY
-    const url = `http://newsapi.org/v2/everything?q=COVID&from=2020-04-31&sortBy=popularity&apiKey=${key}`
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = today.getFullYear();
+    today = yyyy + '/' + mm + '/' + dd;
+
+    const url = `https://newsapi.org/v2/everything?q=COVID&from=${today}&sortBy=publishedAt&apiKey=${key}`
 
     useEffect(() => {
         fetch(url)
